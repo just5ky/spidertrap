14ea855b77d3d4246c118dd9ce44be55702d6720
JustSKY <71321862+Just5KY@users.noreply.github.com>
2022-05-20 17:42:45 +0530

    Create log.py

---
+++ log.py
@@ -0,0 +1,25 @@
+import subprocess as sp
+from concurrent.futures import ThreadPoolExecutor
+
+
+def log_popen_pipe(p, stdfile):
+
+    with open("/var/log/spidertrap", "spidertrap.log", "w") as f:
+
+        while p.poll() is None:
+            f.write(stdfile.readline())
+            f.flush()
+
+        # Write the rest from the buffer
+        f.write(stdfile.read())
+
+
+with sp.Popen(
+    ["python3", "spidertrap.py"], stdout=sp.PIPE, stderr=sp.PIPE, text=True
+) as p:
+
+    with ThreadPoolExecutor(2) as pool:
+        r1 = pool.submit(log_popen_pipe, p, p.stdout)
+        r2 = pool.submit(log_popen_pipe, p, p.stderr)
+        r1.result()
+        r2.result()
